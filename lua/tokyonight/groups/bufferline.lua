ff316db8f45c2cf39eed3a71ed9c82d1d016956b
Sachs Florian <florian@sachs.me>
2026-09-06 17:07:15 +0200

    more tweaks for bufferline

--- lua/tokyonight/groups/bufferline.lua
+++ lua/tokyonight/groups/bufferline.lua
@@ -8,59 +8,67 @@ function M.get(c, opts)
   return {
     BufferLineFill                  = { bg = c.bg_dark },
 
-    -- Inactive buffers: ähnlich dem "master"-Tab im Screenshot
-    BufferLineBackground            = { fg = "#84a0f5", bg = "#3d4260" },
-    BufferLineBufferVisible         = { fg = "#84a0f5", bg = "#3d4260" },
+    -- Inactive buffers:
+    -- ähnlich dem "master"-Segment in der Bottom-Line
+    BufferLineBackground            = { fg = c.blue, bg = c.bg_highlight },
+    BufferLineBufferVisible         = { fg = c.blue, bg = c.bg_highlight },
 
-    -- Active buffer: deutlich heller Tokyo-Night-Blau
+    -- Active buffer:
+    -- deutlich hell hinterlegt
     BufferLineBufferSelected        = { fg = c.bg, bg = c.blue, bold = true },
 
     BufferLineIndicatorSelected     = { fg = c.blue, bg = c.blue },
 
-    BufferLineSeparator             = { fg = c.bg, bg = "#3d4260" },
-    BufferLineSeparatorVisible      = { fg = c.bg, bg = "#3d4260" },
+    BufferLineSeparator             = { fg = c.bg, bg = c.bg_highlight },
+    BufferLineSeparatorVisible      = { fg = c.bg, bg = c.bg_highlight },
     BufferLineSeparatorSelected     = { fg = c.bg, bg = c.blue },
 
-    BufferLineCloseButton           = { fg = "#84a0f5", bg = "#3d4260" },
-    BufferLineCloseButtonVisible    = { fg = "#84a0f5", bg = "#3d4260" },
+    BufferLineCloseButton           = { fg = c.blue, bg = c.bg_highlight },
+    BufferLineCloseButtonVisible    = { fg = c.blue, bg = c.bg_highlight },
     BufferLineCloseButtonSelected   = { fg = c.bg, bg = c.blue },
 
-    BufferLineModified              = { fg = c.yellow, bg = "#3d4260" },
-    BufferLineModifiedVisible       = { fg = c.yellow, bg = "#3d4260" },
+    BufferLineModified              = { fg = c.yellow, bg = c.bg_highlight },
+    BufferLineModifiedVisible       = { fg = c.yellow, bg = c.bg_highlight },
     BufferLineModifiedSelected      = { fg = c.bg, bg = c.blue },
 
-    BufferLineDuplicate             = { fg = "#84a0f5", bg = "#3d4260" },
-    BufferLineDuplicateVisible      = { fg = "#84a0f5", bg = "#3d4260" },
+    BufferLineDuplicate             = { fg = c.blue, bg = c.bg_highlight },
+    BufferLineDuplicateVisible      = { fg = c.blue, bg = c.bg_highlight },
     BufferLineDuplicateSelected     = { fg = c.bg, bg = c.blue },
 
-    BufferLineNumbers               = { fg = "#84a0f5", bg = "#3d4260" },
-    BufferLineNumbersVisible        = { fg = "#84a0f5", bg = "#3d4260" },
+    BufferLineNumbers               = { fg = c.blue, bg = c.bg_highlight },
+    BufferLineNumbersVisible        = { fg = c.blue, bg = c.bg_highlight },
     BufferLineNumbersSelected       = { fg = c.bg, bg = c.blue, bold = true },
 
-    BufferLineDiagnostic            = { fg = "#84a0f5", bg = "#3d4260" },
-    BufferLineDiagnosticVisible     = { fg = "#84a0f5", bg = "#3d4260" },
+    BufferLineDiagnostic            = { fg = c.blue, bg = c.bg_highlight },
+    BufferLineDiagnosticVisible     = { fg = c.blue, bg = c.bg_highlight },
     BufferLineDiagnosticSelected    = { fg = c.bg, bg = c.blue },
 
-    BufferLineError                 = { fg = c.error, bg = "#3d4260" },
-    BufferLineErrorVisible          = { fg = c.error, bg = "#3d4260" },
+    BufferLineError                 = { fg = c.error, bg = c.bg_highlight },
+    BufferLineErrorVisible          = { fg = c.error, bg = c.bg_highlight },
     BufferLineErrorSelected         = { fg = c.error, bg = c.blue },
 
-    BufferLineWarning               = { fg = c.warning, bg = "#3d4260" },
-    BufferLineWarningVisible        = { fg = c.warning, bg = "#3d4260" },
+    BufferLineWarning               = { fg = c.warning, bg = c.bg_highlight },
+    BufferLineWarningVisible        = { fg = c.warning, bg = c.bg_highlight },
     BufferLineWarningSelected       = { fg = c.warning, bg = c.blue },
 
-    BufferLineInfo                  = { fg = c.info, bg = "#3d4260" },
-    BufferLineInfoVisible           = { fg = c.info, bg = "#3d4260" },
+    BufferLineInfo                  = { fg = c.info, bg = c.bg_highlight },
+    BufferLineInfoVisible           = { fg = c.info, bg = c.bg_highlight },
     BufferLineInfoSelected          = { fg = c.info, bg = c.blue },
 
-    BufferLineHint                  = { fg = c.hint, bg = "#3d4260" },
-    BufferLineHintVisible           = { fg = c.hint, bg = "#3d4260" },
+    BufferLineHint                  = { fg = c.hint, bg = c.bg_highlight },
+    BufferLineHintVisible           = { fg = c.hint, bg = c.bg_highlight },
     BufferLineHintSelected          = { fg = c.hint, bg = c.blue },
 
-    BufferLineTab                   = { fg = "#84a0f5", bg = "#3d4260" },
+    BufferLineTab                   = { fg = c.blue, bg = c.bg_highlight },
     BufferLineTabSelected           = { fg = c.bg, bg = c.blue, bold = true },
-    BufferLineTabSeparator          = { fg = c.bg, bg = "#3d4260" },
+
+    BufferLineTabSeparator          = { fg = c.bg, bg = c.bg_highlight },
     BufferLineTabSeparatorSelected  = { fg = c.bg, bg = c.blue },
+
+    BufferLineOffsetSeparator       = { fg = c.bg, bg = c.bg_highlight },
+
+    BufferLineGroupSeparator        = { fg = c.bg, bg = c.bg_highlight },
+    BufferLineGroupLabel            = { fg = c.blue, bg = c.bg_highlight },
   }
 end
 
