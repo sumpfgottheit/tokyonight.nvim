c161de1b823fbbdb66c92bb3f142eed6525fc8bb
Sachs Florian <florian@sachs.me>
2026-09-06 17:24:30 +0200

    bufferline no has better active/inaktive modes

--- lua/tokyonight/groups/bufferline.lua
+++ lua/tokyonight/groups/bufferline.lua
@@ -8,69 +8,67 @@ function M.get(c, opts)
   return {
     BufferLineFill                  = { bg = c.bg_dark },
 
-    -- Inactive buffers:
-    -- ähnlich dem "master"-Segment in der Bottom-Line
-    BufferLineBackground            = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineBufferVisible         = { fg = c.blue, bg = c.bg_highlight },
+    -- Inactive buffers: match lualine's `master` segment.
+    BufferLineBackground            = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineBuffer                = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineBufferVisible         = { fg = c.blue, bg = c.fg_gutter },
 
-    -- Active buffer:
-    -- deutlich hell hinterlegt
-    BufferLineBufferSelected        = { fg = c.bg, bg = c.blue, bold = true },
+    -- Active buffer: match lualine's `NORMAL` segment.
+    BufferLineBufferSelected        = { fg = c.black, bg = c.blue, bold = true },
 
     BufferLineIndicatorSelected     = { fg = c.blue, bg = c.blue },
 
-    BufferLineSeparator             = { fg = c.bg, bg = c.bg_highlight },
-    BufferLineSeparatorVisible      = { fg = c.bg, bg = c.bg_highlight },
+    BufferLineSeparator             = { fg = c.bg, bg = c.fg_gutter },
+    BufferLineSeparatorVisible      = { fg = c.bg, bg = c.fg_gutter },
     BufferLineSeparatorSelected     = { fg = c.bg, bg = c.blue },
 
-    BufferLineCloseButton           = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineCloseButtonVisible    = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineCloseButtonSelected   = { fg = c.bg, bg = c.blue },
+    BufferLineCloseButton           = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineCloseButtonVisible    = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineCloseButtonSelected   = { fg = c.black, bg = c.blue },
 
-    BufferLineModified              = { fg = c.yellow, bg = c.bg_highlight },
-    BufferLineModifiedVisible       = { fg = c.yellow, bg = c.bg_highlight },
-    BufferLineModifiedSelected      = { fg = c.bg, bg = c.blue },
+    BufferLineModified              = { fg = c.yellow, bg = c.fg_gutter },
+    BufferLineModifiedVisible       = { fg = c.yellow, bg = c.fg_gutter },
+    BufferLineModifiedSelected      = { fg = c.black, bg = c.blue },
 
-    BufferLineDuplicate             = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineDuplicateVisible      = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineDuplicateSelected     = { fg = c.bg, bg = c.blue },
+    BufferLineDuplicate             = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineDuplicateVisible      = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineDuplicateSelected     = { fg = c.black, bg = c.blue },
 
-    BufferLineNumbers               = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineNumbersVisible        = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineNumbersSelected       = { fg = c.bg, bg = c.blue, bold = true },
+    BufferLineNumbers               = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineNumbersVisible        = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineNumbersSelected       = { fg = c.black, bg = c.blue, bold = true },
 
-    BufferLineDiagnostic            = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineDiagnosticVisible     = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineDiagnosticSelected    = { fg = c.bg, bg = c.blue },
+    BufferLineDiagnostic            = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineDiagnosticVisible     = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineDiagnosticSelected    = { fg = c.black, bg = c.blue },
 
-    BufferLineError                 = { fg = c.error, bg = c.bg_highlight },
-    BufferLineErrorVisible          = { fg = c.error, bg = c.bg_highlight },
+    BufferLineError                 = { fg = c.error, bg = c.fg_gutter },
+    BufferLineErrorVisible          = { fg = c.error, bg = c.fg_gutter },
     BufferLineErrorSelected         = { fg = c.error, bg = c.blue },
 
-    BufferLineWarning               = { fg = c.warning, bg = c.bg_highlight },
-    BufferLineWarningVisible        = { fg = c.warning, bg = c.bg_highlight },
+    BufferLineWarning               = { fg = c.warning, bg = c.fg_gutter },
+    BufferLineWarningVisible        = { fg = c.warning, bg = c.fg_gutter },
     BufferLineWarningSelected       = { fg = c.warning, bg = c.blue },
 
-    BufferLineInfo                  = { fg = c.info, bg = c.bg_highlight },
-    BufferLineInfoVisible           = { fg = c.info, bg = c.bg_highlight },
+    BufferLineInfo                  = { fg = c.info, bg = c.fg_gutter },
+    BufferLineInfoVisible           = { fg = c.info, bg = c.fg_gutter },
     BufferLineInfoSelected          = { fg = c.info, bg = c.blue },
 
-    BufferLineHint                  = { fg = c.hint, bg = c.bg_highlight },
-    BufferLineHintVisible           = { fg = c.hint, bg = c.bg_highlight },
+    BufferLineHint                  = { fg = c.hint, bg = c.fg_gutter },
+    BufferLineHintVisible           = { fg = c.hint, bg = c.fg_gutter },
     BufferLineHintSelected          = { fg = c.hint, bg = c.blue },
 
-    BufferLineTab                   = { fg = c.blue, bg = c.bg_highlight },
-    BufferLineTabSelected           = { fg = c.bg, bg = c.blue, bold = true },
+    BufferLineTab                   = { fg = c.blue, bg = c.fg_gutter },
+    BufferLineTabSelected           = { fg = c.black, bg = c.blue, bold = true },
 
-    BufferLineTabSeparator          = { fg = c.bg, bg = c.bg_highlight },
+    BufferLineTabSeparator          = { fg = c.bg, bg = c.fg_gutter },
     BufferLineTabSeparatorSelected  = { fg = c.bg, bg = c.blue },
 
-    BufferLineOffsetSeparator       = { fg = c.bg, bg = c.bg_highlight },
+    BufferLineOffsetSeparator       = { fg = c.bg, bg = c.fg_gutter },
 
-    BufferLineGroupSeparator        = { fg = c.bg, bg = c.bg_highlight },
-    BufferLineGroupLabel            = { fg = c.blue, bg = c.bg_highlight },
+    BufferLineGroupSeparator        = { fg = c.bg, bg = c.fg_gutter },
+    BufferLineGroupLabel            = { fg = c.blue, bg = c.fg_gutter },
   }
 end
 
 return M
-
